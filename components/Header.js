a15b75071156863e5c180347edea980fb51a44bf
Lee Castelani <notayogilee@gmail.com>
2021-06-03 14:29:41 -0400

    add context for state mngmt for login and registration

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { useContext } from 'react'
 import Link from 'next/link'
 import styles from '/styles/Header.module.css'
 import Search from '/components/Search'
+import AuthContext from '/context/AuthContext'
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext)
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -21,16 +25,37 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href='/events/add'>
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/account/login'>
-              <a className='btn-secondary btn-icon'> <FaSignInAlt /> Login</a>
-            </Link>
-          </li>
+          {user ? (
+            // if logged in 
+            <>
+              <li>
+                <Link href='/events/add'>
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/account/dashboard'>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button className="btn-secondary btn-icon" onClick={() => logout()}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // if logged out
+            <>
+              <li>
+                <Link href='/account/login'>
+                  <a className='btn-secondary btn-icon'> <FaSignInAlt /> Login</a>
+                </Link>
+              </li>
+            </>
+          )
+          }
+
         </ul>
       </nav>
     </header>
