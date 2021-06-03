d5853c9f2bea22e17075fc5a56b57b3b9a6f37e6
Lee Castelani <notayogilee@gmail.com>
2021-06-03 16:20:34 -0400

    add logout functionality and destroy httpOnly cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -43,7 +43,14 @@ export const AuthProvider = ({ children }) => {
 
   // Logout user
   const logout = async (user) => {
-    console.log('Logout')
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST'
+    })
+
+    if (res.ok) {
+      setUser(null)
+      router.push('/')
+    }
   }
 
   // Check if user is logged in
